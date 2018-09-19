14a53d18b69345f1fa10c0d247a266daac67148a
Frederic.Pillon <frederic.pillon@st.com>
2018-09-19 09:24:01 +0200

    Update example to work without SD detect pin
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- examples/ReadWrite/ReadWrite.ino
+++ examples/ReadWrite/ReadWrite.ino
@@ -11,6 +11,12 @@
 
 #include <STM32SD.h>
 
+// If SD card slot has no detect pin then define it as SD_DETECT_NONE
+// to ignore it. One other option is to call 'SD.begin()' without parameter.
+#ifndef SD_DETECT_PIN
+#define SD_DETECT_PIN SD_DETECT_NONE
+#endif
+
 File myFile;
 
 void setup()
